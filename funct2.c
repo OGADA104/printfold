779888b051edc519501f7420e6e51f37132a2430
MICHAEL <michaelogada104@gmail.com>
2023-06-27 17:05:28 +0000

    my printf modified

--- funct2.c
+++ funct2.c
@@ -135,6 +135,7 @@ int print_reverse(va_list types, char buffer[],
 	}
 	return (count);
 }
+
 /************************* PRINT A STRING IN ROT13 *************************/
 /**
  * print_rot13string - Print a string in rot13.
@@ -146,6 +147,7 @@ int print_reverse(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Numbers of chars printed
  */
+
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -186,5 +188,4 @@ int print_rot13string(va_list types, char buffer[],
 	}
 	return (count);
 }
-
 
